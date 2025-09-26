9219d3708ef32d6d3749a4475a9a5467f957a433
El-sheshtawy <ramyalfe22@gmail.com>
2025-09-26 23:52:47 +0300

    strcture setup at dash and db

--- resources/views/school/school.blade.php
+++ resources/views/school/school.blade.php
@@ -127,8 +127,7 @@
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('js/sweetalert2.min.js') }}"></script>
     <script src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+
     <script src="{{ asset('js/dataTables.buttons.min.js') }}"></script>
     <script src="{{ asset('js/buttons.html5.min.js') }}"></script>
     <script src="{{ asset('js/jszip.min.js') }}"></script>
