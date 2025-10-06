6c4d0efcead9d78817dc50ac61527050873a9044
El-sheshtawy <ramyalfe22@gmail.com>
2025-10-07 00:39:10 +0300

    Working at cache

--- resources/views/admin/special_users/index.blade.php
+++ resources/views/admin/special_users/index.blade.php
@@ -40,7 +40,13 @@
                     @endif
                 </td>
                     <td class="p-1">{{$user->username}}</td>
-                    <td class="p-1">{{$user->defualt_password ?? 'غير محدد'}}</td>
+                    <td class="p-1">
+                        @if($user->defualt_password && !str_starts_with($user->defualt_password, '$2y$'))
+                            {{$user->defualt_password}}
+                        @else
+                            123456
+                        @endif
+                    </td>
                 </tr>
             @endforeach
         </tbody>
