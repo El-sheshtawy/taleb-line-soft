921aad31020af40498d0970e77cc41b7b4d7441b
El-sheshtawy <ramyalfe22@gmail.com>
2025-10-10 14:17:48 +0300

    solve ordeerresource

--- resources/views/teacher/followup_components/followup_record_table.blade.php
+++ resources/views/teacher/followup_components/followup_record_table.blade.php
@@ -4,17 +4,17 @@
             @media (max-width: 768px) {
                 .table th:nth-child(1),
                 .table td:nth-child(1) {
-                    min-width: 95px !important;
+                    min-width: 110px !important;
                     font-size: 10px !important;
                     white-space: nowrap !important;
                     padding: 2px 4px !important;
                 }
                 .table th:nth-child(2),
                 .table td:nth-child(2) {
-                    min-width: 75px !important;
-                    font-size: 10px !important;
+                    min-width: 60px !important;
+                    font-size: 9px !important;
                     white-space: nowrap !important;
-                    padding: 2px 4px !important;
+                    padding: 2px 2px !important;
                 }
                 .table th:nth-child(n+3):nth-child(-n+9),
                 .table td:nth-child(n+3):nth-child(-n+9) {
