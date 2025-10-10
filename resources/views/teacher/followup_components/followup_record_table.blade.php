38d138948f9e76073acd514c45f413eec1f16124
El-sheshtawy <ramyalfe22@gmail.com>
2025-10-10 14:16:35 +0300

    solve ordeerresource

--- resources/views/teacher/followup_components/followup_record_table.blade.php
+++ resources/views/teacher/followup_components/followup_record_table.blade.php
@@ -4,14 +4,14 @@
             @media (max-width: 768px) {
                 .table th:nth-child(1),
                 .table td:nth-child(1) {
-                    min-width: 80px !important;
+                    min-width: 95px !important;
                     font-size: 10px !important;
                     white-space: nowrap !important;
                     padding: 2px 4px !important;
                 }
                 .table th:nth-child(2),
                 .table td:nth-child(2) {
-                    min-width: 90px !important;
+                    min-width: 75px !important;
                     font-size: 10px !important;
                     white-space: nowrap !important;
                     padding: 2px 4px !important;
